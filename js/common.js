6f7aaecc46913044e708c487edf459311e438eaa
hraboviyvadim <hraboviyvadim@gmail.com>
2015-03-23 17:25:54 +0200

    catapult pages responsive upd

--- js/common.js
+++ js/common.js
@@ -52,6 +52,33 @@ head.ready(function() {
 	}
 	slick2();
 	
+	$('.js-slick-3').slick({
+			speed: 600,
+			infinite: true,
+			slidesToShow: 6,
+			slidesToScroll: 1,
+			responsive: [
+			    {
+			    	breakpoint: 768,
+			    	settings: {
+			    		slidesToShow: 3,
+			    	}
+			    },
+			    {
+			    	breakpoint: 569,
+			    	settings: {
+			    		slidesToShow: 2,
+			    	}
+			    },
+			    {
+			    	breakpoint: 481,
+			    	settings: {
+			    		slidesToShow: 1,
+			    	}
+			    }
+			]
+		});
+
 	$('.js-clients').slick({
 			speed: 600,
 			infinite: true,
